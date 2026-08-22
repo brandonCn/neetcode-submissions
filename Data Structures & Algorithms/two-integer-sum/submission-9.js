8c70ae04bd24d142d484547cab8bc45f4fd73b5c
brandonCn <brandon.c.nolan@gmail.com>
2026-08-21 21:57:19 -0500

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let numsMap = {};
+
+        for(let i = 0; i < nums.length; i++){
+            numsMap[nums[i]] = i;
+        }
+
+        for(let i = 0; i < nums.length; i++){
+            const diff = target - nums[i];
+
+            if(numsMap[diff] !== undefined && numsMap[diff] !== i){
+                return [i, numsMap[diff]]
+            }
+        }
+    }
+}
