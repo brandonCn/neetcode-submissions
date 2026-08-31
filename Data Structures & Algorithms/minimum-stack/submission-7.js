65461f99ad3b35da401b746071f6ae8e2855e7d3
brandonCn <brandon.c.nolan@gmail.com>
2026-08-31 16:18:39 -0500

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.js
@@ -0,0 +1,70 @@
+class MinStack {
+    constructor() {
+        this.stack = [];    // main stack
+        this.minStack = []; // min stack.
+
+        // minStack is a verion of the stack maintained in ascending order.
+        // - where smallest values are at the top, so it can be pop'd off.
+        // - the push and pop operations change both stacks so they are in sync in length.
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val); // push current value to main stack.
+        
+        // Compare current push val to the current top of the min stack.
+        // - If the current val is smaller than the top of the min stack, 
+        //   push the current val onto the min stack.
+        //   If the current min top is smaller than current value, just push it to top of
+        //   the min stack again (so the stacks are equal in length for pop operations).
+        val = Math.min(
+            val,
+            (
+                // if minstack is empty, use the current value for compare; 
+                // else, use the current top of min stack.
+                this.minStack.length === 0 
+                ? val
+                : this.minStack[this.minStack.length - 1]
+            )
+        );
+
+        // If minStack is empty, ignore the compare, and just push current val onto the minStack.
+        if(this.minStack.length !== 0){
+
+            // Compare current push val to the current top of the min stack.
+            // - Math.min() picks the smallest of the two values.
+            //   If current val is smaller, push that to minStack;
+            //   else, push the top of minStack to minStack again.
+                 
+            val = Math.min(val, this.minStack[this.minStack.length - 1]);
+        }        
+
+        this.minStack.push(val);        
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        // Pop both to keep in sync.
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
