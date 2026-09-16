b1182b15524270d3d77d81783c028525f805c925
brandonCn <brandon.c.nolan@gmail.com>
2026-09-16 14:11:37 -0500

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.js
@@ -0,0 +1,40 @@
+/**
+ * const { PriorityQueue, MaxPriorityQueue, MinPriorityQueue } = require('@datastructures-js/priority-queue');
+ */
+
+class MedianFinder {
+    constructor() {
+        this.small = new MaxPriorityQueue(); // Max heap for smaller half
+        this.large = new MinPriorityQueue(); // Min heap for larger half
+    }
+
+    /**
+     * @param {number} num
+     */
+    addNum(num) {
+        if (!this.large.isEmpty() && num > this.large.front()) {
+            this.large.enqueue(num);
+        } else {
+            this.small.enqueue(num);
+        }
+
+        if (this.small.size() > this.large.size()) {
+            this.large.enqueue(this.small.dequeue());
+        } else if (this.large.size() > this.small.size()) {
+            this.small.enqueue(this.large.dequeue());
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    findMedian() {
+        if (this.small.size() > this.large.size()) {
+            return this.small.front();
+        } else if (this.large.size() > this.small.size()) {
+            return this.large.front();
+        } else {
+            return (this.small.front() + this.large.front()) / 2.0;
+        }
+    }
+}
