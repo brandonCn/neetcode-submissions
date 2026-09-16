0860db157aba125d4d9c3a2f23d3d00e0a5e0414
brandonCn <brandon.c.nolan@gmail.com>
2026-09-15 20:52:56 -0500

    Add: kth-largest-integer-in-a-stream - submission-2

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-2.js
@@ -0,0 +1,21 @@
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     */
+    constructor(k, nums) {
+        this.arr = nums;
+        this.k = k;
+    }
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val) {
+        this.arr.push(val);
+        this.arr.sort((a, b) => a - b);
+        //this.arr.shift();
+        return this.arr[this.arr.length - this.k];
+    }
+}
