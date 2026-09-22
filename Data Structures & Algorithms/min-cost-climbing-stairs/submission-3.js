76249de4ba0acf9bf5357734d7643f9fdbafc615
brandonCn <brandon.c.nolan@gmail.com>
2026-09-22 18:44:54 -0500

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost) {
+        const stair1 = cost[0];
+        const stair2 = cost[1];
+
+        for(let i = 2; i < cost.length; i++){
+            const temp = cost[i-2];
+            cost[i] = cost[i] + Math.min(cost[i-2], cost[i-1]);
+            cost[i-2] = temp;
+        }
+
+        return Math.min(
+            cost[cost.length - 1],
+            cost[cost.length - 2]
+        )
+    }
+}
