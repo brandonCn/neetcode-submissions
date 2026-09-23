515a83d41b43c02fb932096a2ca4404442235cd8
brandonCn <brandon.c.nolan@gmail.com>
2026-09-22 23:03:58 -0500

    Add: house-robber - submission-13

---
+++ Data Structures & Algorithms/house-robber/submission-13.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        if (nums.length === 0) return 0; // No houses
+        if (nums.length === 1) return nums[0]; // One house
+
+        const houses = new Array(nums.length);
+
+        houses[0] = nums[0];
+        houses[1] = Math.max(nums[0], nums[1]);
+
+        for(let i = 2; i < nums.length; i++){
+            houses[i] = Math.max(houses[i - 1], nums[i] + houses[i - 2]);
+        }   
+
+        return houses[nums.length - 1];     
+    }
+}
